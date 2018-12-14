e542f22a41ae9af9f7874bfe7c821ee6dcab5df0
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2018-12-13 20:24:31 -0500

    first SQL db

---
+++ pokemon table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Pokemon (
+	id integer,
+	name varchar,
+	type varchar,
+	evolves_from_id integer 
+);
+
+insert into pokemon(id,name,type,evolves_from_id)
+VALUES(12,'Butterfree','Bug',11);
